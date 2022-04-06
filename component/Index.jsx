cb968319938303c4d3daadc4790d0c9057da15bd
djalexkidd <alexpyr@outlook.fr>
2022-04-06 14:39:13 +0200

    Bouton

--- component/Index.jsx
+++ component/Index.jsx
@@ -1,9 +1,11 @@
 import React from "react"
+import {BuyButton} from "./Button"
 
 export const App = () => {
     return (
         <div className="main">
             <p>salut</p>
+            <BuyButton />
         </div>
     )
 }
