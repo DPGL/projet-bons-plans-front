208baae4b73646318ab7eff9556c0d4fcc2b39dd
djalexkidd <alexpyr@outlook.fr>
2022-04-07 12:04:25 +0200

    Fixed menu

---
+++ component/FixedMenu.jsx
@@ -0,0 +1,8 @@
+import React from "react"
+import './FixedMenu.css'
+
+export const BuyButton = () => {
+    return (
+        <div></div>
+    )
+}
--- component/Index.jsx
+++ component/Index.jsx
@@ -1,9 +1,11 @@
 import React from "react"
 import {BuyButton} from "./Button"
+import {FixedMenu} from "./FixedMenu"
 
 export const App = () => {
     return (
         <div className="main">
+            <FixedMenu />
             <p>salut</p>
             <BuyButton />
         </div>
