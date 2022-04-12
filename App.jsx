f10503ecf5cc8e46d4e130dd9d50dd00c48c532b
DPGL <villainvictor4@gmail.com>
2022-04-12 15:22:14 +0200

    bug fixed

--- App.jsx
+++ App.jsx
@@ -3,9 +3,13 @@ import ReactDOM from "react-dom"
 import "./public/style/index.css"
 import BP7Dpro from "./Pages/BP7Dpro"
 import Bp4DAnnonceLogement from "./Pages/Bp4DAnnonceLogement"
+import Footer from './component/Footer'
 
 const App = () => {
-    <BP7Dpro />
+return(
+
+    <Bp4DAnnonceLogement />
+)
 }
 
 ReactDOM.render(
--- Pages/Bp4DAnnonceLogement.jsx
+++ Pages/Bp4DAnnonceLogement.jsx
@@ -1,19 +1,21 @@
-import React from "react"
+import React from "react";
 import Sidebar from "../component/Sidebar";
-import Logement from "../component/Bp4DAnnonceLogement"
-import Footer from "../component/Footer"
+import Logement from "../component/Bp4DAnnonceLogement";
+import Footer from "../component/Footer";
 
-import '/pages/Bp4DAnnonceLogement.css'
-import '../component/Main.css'
-
+import "../pages/Bp4DAnnonceLogement.css";
+import "../component/Main.css";
 
 const Bp4DAnnonceLogement = () => {
-    return (
-        <div className="body-header">
-            <Sidebar />
-            <Logement />
-        </div>
-    )
-}
+  return (
+    <div className="page">
+      <div className="Bp4DAnnonceStage">
+        <Sidebar />
+        <Logement />
+      </div>
+      <Footer />
+    </div>
+  );
+};
 
 export default Bp4DAnnonceLogement;
