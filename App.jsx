3ae949911e8d24316866cde7bdd11204575a2cf0
DPGL <villainvictor4@gmail.com>
2022-04-12 14:18:32 +0200

    footer added

--- App.jsx
+++ App.jsx
@@ -1,11 +1,14 @@
 import React from "react"
 import ReactDOM from "react-dom"
+
+import Footer from "./component/Footer"
 import Bp4DAnnonceStage from "./pages/Bp4DAnnonceStage"
 
 const App = () => {
     return (
         <div>
-            <Bp4DAnnonceStage />,
+            <Bp4DAnnonceStage />
+            <Footer />
         </div>
     )
 }
