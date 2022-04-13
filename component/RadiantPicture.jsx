eef3fdc09434ccfe98475fc7904d318ea30bf82a
djalexkidd <alexpyr@outlook.fr>
2022-04-13 09:48:46 +0200

    Photo de profil

--- component/RadiantPicture.jsx
+++ component/RadiantPicture.jsx
@@ -1,8 +1,11 @@
 import React from "react"
 import './RadiantPicture.css'
+import ProfilePicture from '../public/profile-picture.png'
 
 export const RadiantPicture = () => {
     return (
-        <div className="radpict"></div>
+        <div className="radpict">
+            <img className="pfp" src={ProfilePicture} />
+        </div>
     )
 }
